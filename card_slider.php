a03c77ce3d5578669dd1456fb16649d57870b9de
K.N Madushan <115458747+knmadushan@users.noreply.github.com>
2023-04-23 10:43:30 +0530

    card-slider

---
+++ card_slider.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>card_Slider</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <style>
+        .carousel .carousel-item {
+    transition-duration: 10s;
+}
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-12 col-md-6 col-lg-4 col-xl-4">
+                            <div class="card" style="width: 18rem;">
+                                <img src="1.jpg" alt="" class="card-img-top">
+
+                                <div class="card-body">
+                                    <h5>Title</h5>
+                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque quas corrupti eligendi nostrum quibusdam dolores, asperiores velit amet non voluptatum,
+                                        soluta atque exercitationem voluptatibus pariatur obcaecati aspernatur? Delectus, iure porro!</p>
+                                    <a href="" class="btn btn-primary">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-4 col-xl-4">
+                            <div class="card" style="width: 18rem;">
+                                <img src="1.jpg" alt="" class="card-img-top">
+
+                                <div class="card-body">
+                                    <h5>Title</h5>
+                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque quas corrupti eligendi nostrum quibusdam dolores, asperiores velit amet non voluptatum,
+                                        soluta atque exercitationem voluptatibus pariatur obcaecati aspernatur? Delectus, iure porro!</p>
+                                    <a href="" class="btn btn-primary">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-4 col-xl-4">
+                            <div class="card" style="width: 18rem;">
+                                <img src="1.jpg" alt="" class="card-img-top">
+
+                                <div class="card-body">
+                                    <h5>Title</h5>
+                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque quas corrupti eligendi nostrum quibusdam dolores, asperiores velit amet non voluptatum,
+                                        soluta atque exercitationem voluptatibus pariatur obcaecati aspernatur? Delectus, iure porro!</p>
+                                    <a href="" class="btn btn-primary">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-12 col-md-6 col-lg-4 col-xl-4">
+                            <div class="card" style="width: 18rem;">
+                                <img src="1.jpg" alt="" class="card-img-top">
+
+                                <div class="card-body">
+                                    <h5>Title</h5>
+                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque quas corrupti eligendi nostrum quibusdam dolores, asperiores velit amet non voluptatum,
+                                        soluta atque exercitationem voluptatibus pariatur obcaecati aspernatur? Delectus, iure porro!</p>
+                                    <a href="" class="btn btn-primary">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-4 col-xl-4">
+                            <div class="card" style="width: 18rem;">
+                                <img src="1.jpg" alt="" class="card-img-top">
+
+                                <div class="card-body">
+                                    <h5>Title</h5>
+                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque quas corrupti eligendi nostrum quibusdam dolores, asperiores velit amet non voluptatum,
+                                        soluta atque exercitationem voluptatibus pariatur obcaecati aspernatur? Delectus, iure porro!</p>
+                                    <a href="" class="btn btn-primary">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-4 col-xl-4">
+                            <div class="card" style="width: 18rem;">
+                                <img src="1.jpg" alt="" class="card-img-top">
+
+                                <div class="card-body">
+                                    <h5>Title</h5>
+                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque quas corrupti eligendi nostrum quibusdam dolores, asperiores velit amet non voluptatum,
+                                        soluta atque exercitationem voluptatibus pariatur obcaecati aspernatur? Delectus, iure porro!</p>
+                                    <a href="" class="btn btn-primary">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
+
+
+</html>
